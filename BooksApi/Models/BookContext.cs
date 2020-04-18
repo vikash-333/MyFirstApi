eefb8fd86c52315d0e0386a9b84894b9e6a5cfb0
Asus <Asus@LAPTOP-5FHRMNG9>
2020-04-19 05:29:25 +0530

    First commit

---
+++ BooksApi/Models/BookContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksApi.Models
+{
+    public class BookContext:DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
