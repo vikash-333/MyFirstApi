5eea4f647c8bee220be1301c0b06f5fd7e642c8d
Asus <Asus@LAPTOP-5FHRMNG9>
2020-04-19 05:43:39 +0530

    Second Commit

--- BooksApi/Controllers/BooksController.cs
+++ BooksApi/Controllers/BooksController.cs
@@ -43,41 +43,6 @@ namespace BooksApi.Controllers
             return book;
         }
 
-        // PUT: api/Books/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> PutBook(int id, Book book)
-        //{
-        //    if (id != book.Id)
-        //    {
-        //        return BadRequest();
-        //    }
-
-        //    _context.Entry(book).State = EntityState.Modified;
-
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!BookExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
-
-        //    return NoContent();
-        //}
-
-        // POST: api/Books
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
         [Route("/Book")]
         [HttpPost]
         public async Task<ActionResult<Book>> PostBook(Book book)
@@ -88,25 +53,6 @@ namespace BooksApi.Controllers
             return CreatedAtAction(nameof(GetBook), new { id = book.Id }, book);
         }
 
-        //// DELETE: api/Books/5
-        //[HttpDelete("{id}")]
-        //public async Task<ActionResult<Book>> DeleteBook(int id)
-        //{
-        //    var book = await _context.Books.FindAsync(id);
-        //    if (book == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    _context.Books.Remove(book);
-        //    await _context.SaveChangesAsync();
-
-        //    return book;
-        //}
-
-        //private bool BookExists(int id)
-        //{
-        //    return _context.Books.Any(e => e.Id == id);
-        //}
+        
     }
 }
